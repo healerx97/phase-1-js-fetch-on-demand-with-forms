9288cb6e5b202edbd63a4853de7a6f5b2570a198
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-23 20:05:19 -0400

    commit tree b5a18f9fe1d7eabbec8a2756dd3c4530f87edcaf

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        fetch(`http://localhost:3000/movies/${e.target.children[1].value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
